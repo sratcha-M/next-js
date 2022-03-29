b1585eb01363fb13a6dbe6c64d1d128ba858ab7d
sratcha.sae <sratcha.sae@bkp.co.th>
2022-03-30 00:42:14 +0700

    update 404 and react-use

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 const { motion } = require("framer-motion");
+import { useScroll, useWindowScroll } from "react-use";
 
 type Props = {};
 
 const HomePage = (props: Props) => {
+  const scrollRef = React.useRef(null);
+//   const { x, y } = useScroll(scrollRef);
+  const {x, y} = useWindowScroll();
+
+  console.log('scrollRef :>> ', scrollRef);
+  console.log("x,y :>> ", x, y);
   const list = {
     hidden: {
       opacity: 0,
@@ -19,7 +26,7 @@ const HomePage = (props: Props) => {
     },
   };
   return (
-    <div>
+    <div style={{ height: "3000px", width: "100%" }} ref={scrollRef}>
       <Title>test</Title>
       <motion.button
         whileHover={{ scale: 1.1 }}
@@ -44,6 +51,7 @@ const HomePage = (props: Props) => {
         <motion.li variants={item} />
         <motion.li variants={item} />
       </motion.ul>
+      <div>{x + "" + y}</div>
     </div>
   );
 };
